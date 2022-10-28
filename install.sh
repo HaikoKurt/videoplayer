db9bd0a5feed72561de43742a137c9846be15c48
Haiko Emmel <mail@haiko-emmel.de>
2022-10-28 13:15:15 +0200

    Installationsskript angelegt

---
+++ install.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+# add configuration to boot config to start / stop with Pin3 to ground
+ADDCONFIG='dtoverlay=gpio-shutdown,gpio_pin=3,active_low=1,gpio_pull=up'
+FILE=dummy.txt
+if ! grep -q "$ADDCONFIG" "$FILE"; then
+    sed -i.bak -e $'$ a\\\n\\n'"\\$ADDCONFIG" $FILE
+fi
+
+# update system
+sudo apt update -y
+sudo apt upgrade -y
+
+# install omxplayer and usbmount
+sudo apt install omxplayer -y
+sudo apt install usbmount -y
+
+# copy start script and make executable
+cp system/start.sh /home/pi
+chmod +x /home/pi/start.sh
+
+# install and start service
+sudo cp system/videoplayer.service /lib/systemd/system/
+sudo systemctl enable videoplayer.service
+sudo systemctl start videoplayer.service

1cfea60dec24f0d183cda8e8363bcf33c07be484
Haiko Emmel <mail@haiko-emmel.de>
2022-10-28 21:44:03 +0200

    Verbesserungen und Erweiterungen des Skripts
    - keine Wartezeit beim Start des Skripts (unnötig)
    - nutzt Videofiles auf einem USB Stick
    - wertet Einstellungen in der Datei play.txt aus:
    --- once -> video nur einmal spielen (ansonsten Dauerschleife)
    --- copy -> kopiere die Dateien vor dem Start vom USB-Stick auf ein lokales Verzeichnis

--- system/start.sh
+++ system/start.sh
@@ -1,4 +1,40 @@
 #! /bin/bash
 
-sleep 20
-omxplayer --loop -o hdmi /home/pi/VIDEO.mp4
+playfile() {
+    echo "$1"/play.txt
+}
+
+play() {
+    unset OPT
+    PLAYFILE=$(playfile "$1")
+    if test -f "$PLAYFILE" ; then
+        if ! grep -q "once" "$PLAYFILE" ; then
+            OPT="--loop"
+        fi
+    else
+        OPT="--loop"
+    fi
+    VIDEOS=( "$1"/*.mp4 )
+    if test -f "${VIDEOS[0]}" ; then
+        echo "Play: ${VIDEOS[0]} / $OPT"
+        omxplayer $OPT -o hdmi "${VIDEOS[0]}"
+        exit 0
+    fi
+}
+
+USB=/media/usb
+LOCAL=/home/pi/media
+PLAYFILE=$(playfile $USB)
+
+if test -f "$PLAYFILE" ; then
+    if grep -q "copy" "$PLAYFILE" ; then
+        echo "Copy: $USB -> $LOCAL"
+        mkdir -p "$LOCAL"
+        rm -f "$LOCAL"/*
+        cp "$USB"/* "$LOCAL"
+        play "$LOCAL"
+    fi
+fi
+
+play $USB
+play $LOCAL
